03c52f759a339c4e863a785a82bc8ecc5a69c4c6
Cristian Martinez <martinec@users.noreply.github.com>
2016-01-13 17:06:48 +0100

    minor: refactor copyright year

--- vinber.sh
+++ vinber.sh
@@ -3,7 +3,7 @@
 # Vinber: The Unitex/GramLab Build Automation Service
 # https://github.com/UnitexGramLab/vinber-backend
 # =============================================================================
-# Copyright (C) 2015 Université Paris-Est Marne-la-Vallée <unitex-devel@univ-mlv.fr>
+# Copyright (C) 2015-2016 Université Paris-Est Marne-la-Vallée <unitex-devel@univ-mlv.fr>
 # 
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
